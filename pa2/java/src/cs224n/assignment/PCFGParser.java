4d2e74fe8aaf0b93b617eb0ac74b5478765c2b8a
meric <mihail911@gmail.com>
2014-10-12 19:40:14 -0700

    Added pseudocode for CKY algorithm

--- pa2/java/src/cs224n/assignment/PCFGParser.java
+++ pa2/java/src/cs224n/assignment/PCFGParser.java
@@ -1,6 +1,8 @@
 package cs224n.assignment;
 
 import cs224n.ling.Tree;
+import cs224n.util.*;
+
 import java.util.*;
 
 /**
@@ -9,6 +11,7 @@ import java.util.*;
 public class PCFGParser implements Parser {
     private Grammar grammar;
     private Lexicon lexicon;
+
 
     public void train(List<Tree<String>> trainTrees) {
         // TODO: before you generate your grammar, the training trees
@@ -20,11 +23,52 @@ public class PCFGParser implements Parser {
 
         lexicon = new Lexicon(trainTrees);
         grammar = new Grammar(trainTrees);
-        return;
     }
 
     public Tree<String> getBestParse(List<String> sentence) {
         // TODO: implement this method
+        /*
+            CKY Algorithm (words, grammar):
+            score = new double [#(words)+1][#(words-1)][#nonterms]
+            back = new Pair[#(words)+1][#(words)+1][#nonterms]
+            for i=0; i<(words); i++
+                for A in nonterms:
+                    if A -> words[i] in grammar
+                        score[i][i+1][A]=P(A -> words[i])
+                //handle unaries
+                boolean added = true
+                while added
+                    added = false
+                    for A, B in nonterms
+                        if score[i][i+1][B] > 0 && A->B in grammar
+                            prob = p(A->B)*score[i][i+1][B]
+                            if prob > score[i][i+1][A]
+                                score[i][i+1][A] = prob
+                                back[i][i+1][A] = B
+                                added = true
+            for span = 2 to #(words)
+                for begin = 0 to #(words - span
+                    end = begin + span
+                    for split = being+1 to end-1
+                        for A,B,C in nonterms
+                            prob = score[begin][split][B]*score[split[end][C] * P(A->BC)
+                            if prob > score[begin][end][A]
+                                score[begin][end][A] = prob
+                                back[begin][end][A] = new Triple (split,B,C)
+                    //handle unaries
+                    boolean added = true
+                    while added
+                        added = false
+                        for A, B in nonterms
+                            prob = P(A->B)*score[begin][end][B]
+                            if prob > score[begin][end][A]
+                                score[begin][end][A] = prob
+                                back[begin][end][A] = B
+                                added = true
+            return buildTree (score, back) //return most probable parse
+
+
+         */
         return null;
     }
 }
