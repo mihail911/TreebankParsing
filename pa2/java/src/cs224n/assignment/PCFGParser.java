362555f26af34d076b21ba1f9d80ac803e4e641d
meric <mihail911@gmail.com>
2014-10-12 22:17:04 -0700

    started writing CKY algorithm code. ran into some conceptual issues

--- pa2/java/src/cs224n/assignment/PCFGParser.java
+++ pa2/java/src/cs224n/assignment/PCFGParser.java
@@ -23,10 +23,63 @@ public class PCFGParser implements Parser {
 
         lexicon = new Lexicon(trainTrees);
         grammar = new Grammar(trainTrees);
+        return;
     }
 
     public Tree<String> getBestParse(List<String> sentence) {
         // TODO: implement this method
+        int numWords = sentence.size();
+        //Initialize scores array
+        ArrayList<ArrayList<Counter<String>>> scores = new ArrayList<ArrayList<Counter<String>>>();
+        for(int i = 0; i <= numWords; ++i){
+            scores.add(new ArrayList<Counter<String>>());
+            for(int j = 0; j <= numWords; ++j){
+                scores.get(i).add(new Counter<String>());
+            }
+        }
+        //Initialize backPointer array
+        ArrayList<ArrayList<Counter<Pair<Integer,Integer>>>> backPointers = new ArrayList<ArrayList<Counter<Pair<Integer,Integer>>>>();
+        for(int i = 0; i <= numWords; ++i){
+            backPointers.add(new ArrayList<Counter<Pair<Integer,Integer>>>());
+            for(int j = 0; j <= numWords; ++j){
+                backPointers.get(i).add(new Counter<Pair<Integer,Integer>>());
+            }
+        }
+
+        for(int i = 0; i < numWords; ++i){
+            String word = sentence.get(i);
+            for (String tag: lexicon.getAllTags()){
+                if(lexicon.isKnown(word) && lexicon.wordToTagCounters.getCount(word,tag) != 0){
+                    double prob = lexicon.scoreTagging(word,tag);
+                    scores.get(i).get(i+1).setCount(tag,prob);
+                }
+            }
+            boolean added = true;
+            while (added){
+                added = false;
+                Set<String> children = grammar.unaryRulesByChild.keySet();
+                //Iterating over B in A->B
+                for(String child: children){
+                    List<Grammar.UnaryRule> unaryRules = grammar.getUnaryRulesByChild(child);
+                    //Iterate over all A
+                    for(Grammar.UnaryRule rule: unaryRules){
+                        String parent = rule.parent;
+
+                        double scoreChild = scores.get(i).get(i+1).getCount(child);
+                        if(scoreChild > 0){
+                            double prob = rule.getScore() * scoreChild;
+                            double scoreParent = scores.get(i).get(i+1).getCount(parent);
+                            if(prob > scoreParent){
+                                scores.get(i).get(i+1).setCount(parent,prob);
+
+                                added = true;
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
         /*
             CKY Algorithm (words, grammar):
             score = new double [#(words)+1][#(words-1)][#nonterms]
@@ -49,7 +102,7 @@ public class PCFGParser implements Parser {
             for span = 2 to #(words)
                 for begin = 0 to #(words - span
                     end = begin + span
-                    for split = being+1 to end-1
+                    for split = begin+1 to end-1
                         for A,B,C in nonterms
                             prob = score[begin][split][B]*score[split[end][C] * P(A->BC)
                             if prob > score[begin][end][A]
